3032fd63d182963ec475d2445f2ff48d570c115b
DariaFerbey <dariaferby@gmail.com>
2021-09-22 21:31:02 +0300

    fixed bug

--- src/main/java/com/softserve/model/Hotel.java
+++ src/main/java/com/softserve/model/Hotel.java
@@ -2,12 +2,14 @@ package com.softserve.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Data
+@ToString
 @NoArgsConstructor
 @Table(name = "hotels")
 public class Hotel {
@@ -18,12 +20,16 @@ public class Hotel {
     @Column(name = "hotel_name")
     private String hotelName;
 
+    @ToString.Exclude
     @ManyToOne
     @JoinColumn(name = "country_id")
     private Country country;
 
+    @ToString.Exclude
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "hotel", fetch = FetchType.EAGER)
     private List<Room> rooms;
+
+
 
 }
 
